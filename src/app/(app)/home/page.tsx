4bf305ffe4902b3bcc44d07f1d27d4df703f399a
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-06-26 11:18:17 +0530

    minor design fixes

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -6,15 +6,15 @@ function page() {
   return (
     <div className="h-[100vh] w-[100vw] bg-fields">
       <div className="h-[100%] w-[100vw] md:p-5 md:flex glass">
-        <div className="md:w-1/2 h-[100%] bg-white rounded-l-2xl overflow-hidden">
-          <div className="md:pr-2 md:mt-5">
+        <div className="md:w-1/2 h-[100%] bg-white md:rounded-l-2xl overflow-scroll  no-scrollbar">
+          <div className="md:pr-2 md:mt-5 sticky top-0">
             <Navbar />
           </div>
-          <div className="flex justify-center align-middle items-center h-[90%] bg-white">
+          <div className="flex justify-center align-middle items-center h-auto bg-white">
             <Main />
           </div>
         </div>
-        <div className="md:w-1/2 bg-white h-[100%] p-2 rounded-r-2xl">
+        <div className="md:w-1/2 bg-white h-[100%] p-2 md:rounded-r-2xl">
             <div className=" bg-fields bg-center bg-cover h-[100%] rounded-2xl">
 
             </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,17 +3,17 @@ import { Button } from "@/components/ui/button"
 
 function Main() {
   return (
-    <div className='md:m-9 ml-5 mr-5 flex flex-col '>
-      <div className='mb-5'>
-        <h1 className='text-sm md:text-lg'>EMPOWERING FARMERS WITH CUTTING-EDGE AI TECHNOLOGY</h1>
+    <div className='md:m-9 ml-5 mr-5 flex flex-col  '>
+      <div className='mb-5 mt-5'>
+        <h1 className='text-sm md:text-sm mb-5'>EMPOWERING FARMERS WITH CUTTING-EDGE AI TECHNOLOGY</h1>
       </div>
       <div className='flex flex-col'>
-        <span className='text-4xl md:text-6xl font-light'>AI-POWERED</span> <br /> 
-        <span className='text-4xl md:text-6xl font-semibold'>AGRICULTURE AT</span> <br />
-        <span className='text-4xl md:text-6xl font-semibold'>YOUR FINGERTIPS</span> <br />
+        <span className='text-4xl md:text-[3.7vw] font-light'>AI-POWERED</span> <br /> 
+        <span className='text-4xl md:text-[3.7vw] font-semibold'>AGRICULTURE AT</span> <br />
+        <span className='text-4xl md:text-[3.7vw] font-semibold'>YOUR FINGERTIPS</span> <br />
       </div>
-      <div className='md:w-3/4 mb-5 mt-5'>
-        <span className='text-sm md:text-lg'>Enter details of your soil and weather, and find best suitable crop for farming. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, accusamus!</span>
+      <div className='md:w-3/4 mt-5'>
+        <span className='text-sm md:text-sm'>Enter details of your soil and weather, and find best suitable crop for farming. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, accusamus!</span>
       </div>
       <div className='mt-10'>
         <span className='p-3 pr-14 pl-14 text-black border-2 border-black rounded-3xl font-semibold'>Try Now</span>
