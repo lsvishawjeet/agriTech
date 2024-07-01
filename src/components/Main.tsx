93279eff296b3fb05bf4fc059d3ad4db33f1de47
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-07-01 17:41:17 +0530

    intro update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -21,7 +21,7 @@ function Main({disabled}:any) {
         <span className='text-4xl md:text-[3.7vw] font-semibold'>YOUR FINGERTIPS</span> <br />
       </div>
       <div className='md:w-3/4 mt-5'>
-        <span className='text-sm md:text-sm'>Enter details of your soil and weather, and find best suitable crop for farming. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, accusamus!</span>
+        <span className='text-sm md:text-sm'>Enter details of your soil and weather, and find best suitable crop for farming. With our advanced AI system, you can make informed decisions and optimize your agricultural practices.</span>
       </div>
       <div className='mt-10'>
         <Button onClick={clicked} className={ disabled ? "hidden" : 'p-3 pr-14 pl-14 text-black border-2 border-black rounded-3xl font-semibold bg-white hover:text-white'}>Try Now</Button>
