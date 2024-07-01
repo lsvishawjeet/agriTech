47a5452e324b131061a551e470a20d8ba5ff4cc8
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-07-01 17:55:13 +0530

    added vercel speed insights

--- src/app/(app)/insights/page.tsx
+++ src/app/(app)/insights/page.tsx
@@ -9,138 +9,140 @@ function page() {
         <div className="h-[100%] w-[100%] bg-white md:rounded-2xl overflow-scroll  no-scrollbar">
           <div className="md:pr-2 md:mt-5 sticky top-0">
             <Navbar />
-          <div className="flex flex-wrap justify-center align-middle items-center h-auto bg-white">
-            <p className="text-xl p-3">
-              Explore all the useful insights generated from{" "}
-              <a
-                href="https://www.kaggle.com/datasets/aksahaha/crop-recommendation/"
-                target="_blank"
-                className="  md:hover:bg-transparent md:border-0 text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                crop recommendation
-              </a>{" "}
-              dataset:
-            <span className="text-sm pl-2 font-light">please wait this usually take {"<"} min</span>
-            </p>
-          </div>
+            <div className="flex flex-wrap justify-center align-middle items-center h-auto bg-white">
+              <p className="text-xl p-3">
+                Explore all the useful insights generated from{" "}
+                <a
+                  href="https://www.kaggle.com/datasets/aksahaha/crop-recommendation/"
+                  target="_blank"
+                  className="  md:hover:bg-transparent md:border-0 text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  crop recommendation
+                </a>{" "}
+                dataset:
+                <span className="text-sm pl-2 font-light">
+                  please wait this usually take {"<"} min
+                </span>
+              </p>
+            </div>
           </div>
           <div className="flex flex-wrap justify-center space-y-5 p-3">
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/cropdistributiongraph`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Apple`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Banana`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Blackgram`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/ChickPea`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Coconut`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Coffee`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Cotton`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Grapes`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Jute`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/KidneyBeans`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Lentil`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Maize`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Mango`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/MothBeans`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/MungBeans`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Muskmelon`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Orange`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Papaya`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/PigeonPeas`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Pomegranate`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Rice`}
-            alt={``}
-          />
-          <img
-            className="rounded-xl bg-blend-multiply"
-            src={`https://agritechbackendflask.onrender.com/insights/Watermelon`}
-            alt={``}
-          />
-        </div>
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/cropdistributiongraph`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Apple`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Banana`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Blackgram`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/ChickPea`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Coconut`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Coffee`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Cotton`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Grapes`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Jute`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/KidneyBeans`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Lentil`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Maize`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Mango`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/MothBeans`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/MungBeans`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Muskmelon`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Orange`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Papaya`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/PigeonPeas`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Pomegranate`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Rice`}
+              alt={``}
+            />
+            <img
+              className="rounded-xl bg-blend-multiply"
+              src={`https://agritechbackendflask.onrender.com/insights/Watermelon`}
+              alt={``}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}<Toaster /><Analytics/></body>
+      <body className={inter.className}>{children}<Toaster /><Analytics/><SpeedInsights/></body>
     </html>
   );
 }
