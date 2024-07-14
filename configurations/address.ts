bbc4c81fe1bab06fcffc127489af52231fc13dca
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-07-14 20:16:23 +0530

    backend deployment changes

--- configurations/address.ts
+++ configurations/address.ts
@@ -1,1 +1,2 @@
-export const backend_address = "https://agritechbackendflask.onrender.com/"
+// export const backend_address = "https://agritechbackendflask.onrender.com/"
+export const backend_address = "https://agritechbackend.choreoapps.dev/"
