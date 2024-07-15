9c3289650837b0d6c513269f1674750696f8f885
lsvishawjeet <sidhuvishaw999@gmail.com>
2024-07-15 18:24:00 +0530

    backend updated to koyeb

--- configurations/address.ts
+++ configurations/address.ts
@@ -1,2 +1,3 @@
 // export const backend_address = "https://agritechbackendflask.onrender.com/"
-export const backend_address = "https://agritechbackend.choreoapps.dev/"
+// export const backend_address = "https://agritechbackend.choreoapps.dev/"
+export const backend_address = "https://revolutionary-cow-vishawjeet-04e58232.koyeb.app/"
